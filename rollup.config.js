83db2f196b2c88df5af30fabe257d4230433ca91
realfakenerd <lucas210898@hotmail.com>
2021-04-30 01:34:44 +0000

    adding bulma

--- rollup.config.js
+++ rollup.config.js
@@ -8,7 +8,7 @@ import babel from '@rollup/plugin-babel';
 import { terser } from 'rollup-plugin-terser';
 import config from 'sapper/config/rollup.js';
 import pkg from './package.json';
-
+import css from 'rollup-plugin-css-only'
 const mode = process.env.NODE_ENV;
 const dev = mode === 'development';
 const legacy = !!process.env.SAPPER_LEGACY_BUILD;
@@ -35,7 +35,7 @@ export default {
 					dev,
 					hydratable: true
 				}
-			}),
+			}), css({ output : 'static/extra.css' }),
 			url({
 				sourceDir: path.resolve(__dirname, 'src/node_modules/images'),
 				publicPath: '/client/'
